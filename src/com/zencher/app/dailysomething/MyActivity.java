8d7db572c74eacfd4f21b59c05d82669ec75d001
Michael Pan <scentsome@gmail.com>
2015-08-10 16:03:16 +0800

    add a new IntelliJ IDEA Project

---
+++ src/com/zencher/app/dailysomething/MyActivity.java
@@ -0,0 +1,15 @@
+package com.zencher.app.dailysomething;
+
+import android.app.Activity;
+import android.os.Bundle;
+
+public class MyActivity extends Activity {
+    /**
+     * Called when the activity is first created.
+     */
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.main);
+    }
+}
