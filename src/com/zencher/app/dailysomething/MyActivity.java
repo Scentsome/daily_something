2dcb42999c91af16fd6d3409e4028ce0e3a02e92
alan <alanwei8415@gmail.com>
2015-08-10 16:49:58 +0800

    test by alen

--- src/com/zencher/app/dailysomething/MyActivity.java
+++ src/com/zencher/app/dailysomething/MyActivity.java
@@ -5,11 +5,13 @@ import android.os.Bundle;
 
 public class MyActivity extends Activity {
     /**
-     * Called when the activity is first created.
-     */
+     * Called when the activity is first created. by Alen
+    */
     @Override
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.main);
+
+        int num = 9;
     }
 }
